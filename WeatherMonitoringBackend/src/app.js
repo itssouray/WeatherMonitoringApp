f401b46cd1cf757db20ae311665131c383158d75
sourav <itssouraygupta.com>
2024-10-18 19:54:57 +0530

    cors policy update

--- WeatherMonitoringBackend/src/app.js
+++ WeatherMonitoringBackend/src/app.js
@@ -3,9 +3,12 @@ const express = require("express");
 const router = require("./router/weather.router");
 
 const app = express();
-
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+}));
 app.use(express.json());
-app.use(cors());
+
 app.use("/api/weather",router);
 
 
